c0256797c8b4fd5c645e5decff2bac44b0117db1
Asen-Armando <borisasen@gmail.com>
2019-03-28 19:48:38 -0500

    asd

--- Tp3.cpp
+++ Tp3.cpp
@@ -1,25 +1,53 @@
 //Comentario de prueba grupo1
 
-enum TRaza{Orco, Humano, Mago, Enano, Elfo}
-char Nombres[6][10]={“ale”, “b”, “c”, “d”, “e”}
-char Apellidos[6][10]={“f”, “g”, “hormiga”, “i”, “j”}
+enum TRaza{Orco, Humano, Mago, Enano, Elfo};
+char Nombres[6][10]={"ale", "b", "c", "d", "e"};
+char Apellidos[6][10]={"f", "g", "hormiga", "i", "j"};
 
-struct TDatos {
+typedef struct{
 	TRaza Raza; //nota 1
-	Char *ApellidoNombre; //nota 2
+	char *ApellidoNombre; //nota 2
 	int edad; //entre 0 a 300
-	double Salud://100
-};
+	double Salud;//100
+} TDatos;
 
-struct TCaracteristicas{
+typedef struct {
 	int velocidad;// 1 a 10
 	int destreza; //1 a 5
 	int fuerza;//1 a 10
 	int Nivel; //1 a 10
 	int Armadura; //1 a 10
-};
+}TCaracteristicas;
 
-struct TPersonaje {
-	TDatos * DatosPersonales
-	TCaracteristicas * Caracteristicas
-};
+typedef struct  {
+	TDatos * DatosPersonales;
+	TCaracteristicas * Caracteristicas;
+}TPersonaje;
+
+#include<stdio.h>
+#include<stdlib.h>
+
+TDatos cargar_datos();
+
+int main(){
+	printf("Ingrese candidad de personajes a introducir: ");
+	scanf("%d",&cantp);
+	//int cantper;
+	//TPersonaje* personajes=(TPersonaje*)malloc(sizeof(TPersonaje)*cantper);
+	TPersonaje* personaje1;
+	personaje1->DatosPersonales = cargar_datos();
+	//personaje1.Raza = Mago;
+
+	return 0;
+}
+
+TDatos cargar_datos(){
+	DatosPersonales = (TDatos*)malloc(sizeof(TDatos));
+	DatosPersonales->Raza= Mago;
+	DatosPersonales->ApellidoNombre="asd";
+	DatosPersonales->edad=10;
+	DatosPersonales->Salud=100.50;
+
+	return personaje;
+
+}
