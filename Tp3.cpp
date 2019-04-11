9ad4713463da6109c464439e547a17741b88e56a
BorisAsen <borisyasen@gmail.com>
2019-04-11 02:10:05 -0300

    Version mas interactiva

--- Tp3.cpp
+++ Tp3.cpp
@@ -46,6 +46,7 @@ void MostrarCarcteristicas(Personaje * pj, int i);
 void Batalla(Personaje pj1, Personaje pj2);
 void VisorPersonaje(Personaje * pj, int i);
 void Versus();
+void Ataque(int pj1, int pj2);
 
 /***********************PROGRAMA PRINCIPAL***********************/
 
@@ -56,30 +57,26 @@ void main(){
 	Personaje* pj;
 	int cant, pj1, pj2;
 
-	printf("*Ingrese la cantidad de personajes: ");
+	printf("*Ingrese la cantidad de personajes aleatorios a crear: ");
 	scanf("%i",&cant);
 	pj = (Personaje*)malloc(sizeof(Personaje)*cant);
 
-	printf("\n*Personajes disponibles: \n");
+	printf("\n\n*Personajes disponibles: \n");
 	MostrarPersonajes(pj,cant);
 
-	printf("\nEliga 2 personajes para luchar entre si\n");
+	printf("\n\n* Eliga 2 personajes para luchar entre si\n");
 	printf("\tIngrese el numero del primer personaje: ");
 	scanf("%i",&pj1);
 	printf("\tIngrese el numero del segundo personaje: ");
 	scanf("%i",&pj2);
 
 
-	printf("\r\n* %s VS %s\n\n", pj[pj1-1].DatosPersonales->ApellidoNombre, pj[pj2-1].DatosPersonales->ApellidoNombre);
+	printf("\n\n* Personajes Elegidos: \n");
 	VisorPersonaje(pj,pj1-1);
 	Versus();
 	VisorPersonaje(pj,pj2-1);
-
-	printf("\n* Presione una tecla para ver el resultado de la batalla\n");
-	getch();
+	printf("\n\n");
 	Batalla(pj[pj1-1],pj[pj2-1]);
-
-
 }
 
 
@@ -130,27 +127,27 @@ void CargarCarcteristicas(Personaje * pj, int i){
 
 void MostrarDatos(Personaje * pj, int i){
 	
-	cprintf("\r\n%i-%s\n", i+1,pj[i].DatosPersonales->ApellidoNombre);
+	printf("\n%i-%s\n", i+1,pj[i].DatosPersonales->ApellidoNombre);
 	switch(pj[i].DatosPersonales->raza){
-		case 0: cprintf("\r   Raza: Asesino\n"); break;
-		case 1: cprintf("\r   Raza: Tanque\n"); break;
-		case 2: cprintf("\r   Raza: Luchador\n"); break;
-		case 3: cprintf("\r   Raza: Mago\n"); break;
-		case 4: cprintf("\r   Raza: Apoyo\n"); break;
+		case 0: printf("\tRaza: Asesino\n"); break;
+		case 1: printf("\tRaza: Tanque\n"); break;
+		case 2: printf("\tRaza: Luchador\n"); break;
+		case 3: printf("\tRaza: Mago\n"); break;
+		case 4: printf("\tRaza: Apoyo\n"); break;
 	}
-	cprintf("\r   Edad: %i\n", pj[i].DatosPersonales->edad);
-	cprintf("\r   Salud: %.2f\n", pj[i].DatosPersonales->salud);
+	printf("\tEdad: %i\n", pj[i].DatosPersonales->edad);
+	printf("\tSalud: %.2f\n", pj[i].DatosPersonales->salud);
 
 	return;
 }
 
 void MostrarCarcteristicas(Personaje * pj, int i){
 
-	cprintf("\r   Velocidad: %i\n", pj[i].Caract->velocidad);
-	cprintf("\r   Destreza: %i\n", pj[i].Caract->destreza);
-	cprintf("\r   Fuerza: %i\n", pj[i].Caract->fuerza);
-	cprintf("\r   Nivel: %i\n", pj[i].Caract->nivel);
-	cprintf("\r   Armadura: %i\n", pj[i].Caract->armadura);
+	printf("\tVelocidad: %i\n", pj[i].Caract->velocidad);
+	printf("\tDestreza: %i\n", pj[i].Caract->destreza);
+	printf("\tFuerza: %i\n", pj[i].Caract->fuerza);
+	printf("\tNivel: %i\n", pj[i].Caract->nivel);
+	printf("\tArmadura: %i\n", pj[i].Caract->armadura);
 
 	return;
 }
@@ -173,92 +170,95 @@ void Batalla(Personaje pj1, Personaje pj2){
 	float PD,ED,VA,PDEF,MDP=5000;
 
 	s2=100;
-	for (int i = 0; i < 3; ++i)
-	{
-		PD = pj1.Caract->destreza*pj1.Caract->fuerza*pj1.Caract->nivel;
-		PDEF = pj2.Caract->armadura*pj2.Caract->velocidad;
-		ED = (float)(1+random(100))/100;
-		VA = PD * ED;
-		dmg = ((VA-PDEF)/MDP)*100;
-		if (dmg<0){dmg=0;}
-		s2 = s2-dmg;
-		printf("\nPj1-Turno %i: Dmg=%.2f - Salud Pj2=%.2f", i+1, dmg, s2);
-	}
-
-	s1=100;
-	printf("\n");
-
-	for (int i = 0; i < 3; ++i)
-	{
-		PD = pj2.Caract->destreza*pj2.Caract->fuerza*pj2.Caract->nivel;
-		PDEF = pj1.Caract->armadura*pj1.Caract->velocidad;
-		ED = (float)(1+random(100))/100;
-		VA = PD * ED;
-		dmg = ((VA-PDEF)/MDP)*100;
-		if (dmg<0){dmg=0;}
-		s1 = s1-dmg;
-		printf("\nPj2-Turno %i: Dmg=%.2f - Salud Pj1=%.2f", i+1, dmg, s1);
+	for (int i = 0; i < 6; ++i){
+		if (i%2==0){
+			PD = pj1.Caract->destreza*pj1.Caract->fuerza*pj1.Caract->nivel;
+			PDEF = pj2.Caract->armadura*pj2.Caract->velocidad;
+			ED = (float)(1+random(100))/100;
+			VA = PD * ED;
+			dmg = ((VA-PDEF)/MDP)*100;
+			if (dmg<0){dmg=0;}
+			s2 = s2-dmg;
+			printf("\nJUGADOR 1,presiones una tecla para atacar\n");
+			getch();
+			Ataque(1,2);
+			printf("\n\t\t\tDa%co=%.2f  -  Salud Pj2=%.2f\n",164, dmg, s2);
+		}else{
+			s1=100;
+			PD = pj2.Caract->destreza*pj2.Caract->fuerza*pj2.Caract->nivel;
+			PDEF = pj1.Caract->armadura*pj1.Caract->velocidad;
+			ED = (float)(1+random(100))/100;
+			VA = PD * ED;
+			dmg = ((VA-PDEF)/MDP)*100;
+			if (dmg<0){dmg=0;}
+			s1 = s1-dmg;
+			printf("\nJUGADOR 2,presiones una tecla para atacar\n");
+			getch();
+			Ataque(2,1);
+			printf("\n\t\t\tDa%co=%.2f  -  Salud Pj1=%.2f\n",164, dmg, s1);
+		}
+		
 	}
 
 	if (s1>s2){
-		printf("\n\n\t\t  _____________________\n");
-		printf("\t\t |                     |\n");
-		printf("\t\t |  GANA EL JUGADOR 1  |");
-		printf("\n\t\t |_____________________|\n");
+		printf("\n\n\t\t\t  _____________________\n");
+		printf("\t\t\t |                     |\n");
+		printf("\t\t\t |  GANA EL JUGADOR 1  |");
+		printf("\n\t\t\t |_____________________|\n\n\n");
 	}else if(s2>s1){
-		printf("\n\n\t\t  _____________________\n");
-		printf("\t\t |                     |\n");
-		printf("\t\t |  GANA EL JUGADOR 2  |");
-		printf("\n\t\t |_____________________|\n");
+		printf("\n\n\t\t\t  _____________________\n");
+		printf("\t\t\t |                     |\n");
+		printf("\t\t\t |  GANA EL JUGADOR 2  |");
+		printf("\n\t\t\t |_____________________|\n\n\n");
 	}else{
-		printf("\n\n\t\t  _____________________\n");
-		printf("\t\t |                     |\n");
-		printf("\t\t |    HUBO UN EMPATE   |");
-		printf("\n\t\t |_____________________|\n");
+		printf("\n\n\t\t\t  _____________________\n");
+		printf("\t\t\t |                     |\n");
+		printf("\t\t\t |    HUBO UN EMPATE   |");
+		printf("\n\t\t\t |_____________________|\n\n\n");
 	}
 }
 
 
 void VisorPersonaje(Personaje * pj, int i){
 
-	printf("\n \t\t ================================");
-	printf("\r\n\t\t |\t%s \t\t\n",pj[i].DatosPersonales->ApellidoNombre);
+	printf("\n \t\t\t ================================");
+	printf("\n\t\t\t |\t%s \t\t\n",pj[i].DatosPersonales->ApellidoNombre);
+	printf("\t\t\t |\t\t\t\t|\n", pj[i].DatosPersonales->edad);
 	switch(pj[i].DatosPersonales->raza){
-		case 0: printf("\r\t\t |\tRaza: Asesino \t\t|\n"); break;
-		case 1: printf("\r\t\t |\tRaza: Tanque \t\t|\n"); break;
-		case 2: printf("\r\t\t |\tRaza: Luchador \t\t|\n"); break;
-		case 3: printf("\r\t\t |\tRaza: Mago \t\t|\n"); break;
-		case 4: printf("\r\t\t |\tRaza: Apoyo \t\t|\n"); break;
+		case 0: printf("\t\t\t |\tRaza: Asesino \t\t|\n"); break;
+		case 1: printf("\t\t\t |\tRaza: Tanque \t\t|\n"); break;
+		case 2: printf("\t\t\t |\tRaza: Luchador \t\t|\n"); break;
+		case 3: printf("\t\t\t |\tRaza: Mago \t\t|\n"); break;
+		case 4: printf("\t\t\t |\tRaza: Apoyo \t\t|\n"); break;
 	}
-	printf("\r\t\t |\tEdad: %i \t\t|\n", pj[i].DatosPersonales->edad);
-	printf("\r\t\t |\tSalud: %.2f \t\t|\n", pj[i].DatosPersonales->salud);
-	printf("\r\t\t |\tVelocidad: %i \t\t|\n", pj[i].Caract->velocidad);
-	printf("\r\t\t |\tDestreza: %i \t\t|\n", pj[i].Caract->destreza);
-	printf("\r\t\t |\tFuerza: %i \t\t|\n", pj[i].Caract->fuerza);
-	printf("\r\t\t |\tNivel: %i \t\t|\n", pj[i].Caract->nivel);
-	printf("\r\t\t |\tArmadura: %i \t\t|\n", pj[i].Caract->armadura);
-	printf("\t\t ================================\n\n");
+	printf("\t\t\t |\tEdad: %i \t\t|\n", pj[i].DatosPersonales->edad);
+	printf("\t\t\t |\tSalud: %.2f \t\t|\n", pj[i].DatosPersonales->salud);
+	printf("\t\t\t |\tVelocidad: %i \t\t|\n", pj[i].Caract->velocidad);
+	printf("\t\t\t |\tDestreza: %i \t\t|\n", pj[i].Caract->destreza);
+	printf("\t\t\t |\tFuerza: %i \t\t|\n", pj[i].Caract->fuerza);
+	printf("\t\t\t |\tNivel: %i \t\t|\n", pj[i].Caract->nivel);
+	printf("\t\t\t |\tArmadura: %i \t\t|\n", pj[i].Caract->armadura);
+	printf("\t\t\t ================================\n");
 
 	return;
 }
 
 void Versus(){
-
-	printf("\n\n\r\t\t     ||        ||        ====            \n");
-		printf("\r\t\t     ||        ||      //    \\\\       \n");
-		printf("\r\t\t     ||        ||     //      \\\\    \n");
-		printf("\r\t\t     ||        ||     \\\\           \n");
-		printf("\r\t\t     ||        ||      \\\\         \n");
-		printf("\r\t\t     ||        ||       \\\\       \n");
-		printf("\r\t\t      \\\\      //          \\\\     \n");
-		printf("\r\t\t       \\\\    //            \\\\   \n");
-		printf("\r\t\t        \\\\  //              // \n");
-		printf("\r\t\t         \\\\//       \\\\     //\n");
-		printf("\r\t\t          \\/          =====\n");
-		printf("\n\n");
-
-	
-	
-
+		printf("\t\t\t                     ___        \n");
+		printf("\t\t\t        ||    ||   //   \\\\    \n");
+		printf("\t\t\t        ||    ||  ||    ||      \n");
+		printf("\t\t\t        ||    ||   \\\\___      \n");
+		printf("\t\t\t         \\\\  //         \\\\  \n");
+		printf("\t\t\t          \\\\//    ||    ||    \n");
+		printf("\t\t\t           \\/      \\\\__//    \n");
 	return;
+}
+
+void Ataque(int pj1, int pj2){
+
+	printf("\n\n");
+	printf("\t\t    ___                       ___      ___            ");
+  printf("\t\t\t\t   (-.-)_/............  \\|/  | %i | |__(-.-)        \n",pj2);
+	printf("\t\t _/|.%i.|                /|\\   \\_/     |.%i.|\\_  \n",pj1,pj2);
+	printf("\t\t   _| |_                              _| |_         \n");
 }
